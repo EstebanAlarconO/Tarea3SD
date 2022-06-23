4a828258bc0c6c8997f1a820567e4056f4331484
EstebanAlarconO <esteban.alarcon1@mail.udp.cl>
2022-06-22 23:36:38 -0400

    api rest update y delete

--- Cliente/app.py
+++ Cliente/app.py
@@ -22,6 +22,7 @@ def create_receta():
     pacientes, recetas = cassandra()
     if request.method == 'POST':
         
+        #Obtención datos formulario.
         nombre = request.form['nombre']
         apellido = request.form['apellido']
         rut = request.form['rut']
@@ -31,51 +32,84 @@ def create_receta():
         farmacos = request.form['farmacos']
         doctor = request.form['doctor']
 
-        
-        query = "SELECT rut FROM pacientes.paciente;"
+        #Obtención de pacientes ya ingresados
+        query = "SELECT id, rut FROM pacientes.paciente;"
         rows = pacientes.execute(query)
         lista_ruts = []
+        max_id_paciente = 0
         for row in rows:
             lista_ruts.append(row.rut)
+            if(max_id_paciente < row.id):
+                max_id_paciente = row.id
         
-        if(rut not in lista_ruts):
-            query = "INSERT INTO pacientes.paciente(id, nombre, apellido, rut, email, fecha_nacimiento) VALUES(now(), ?, ?, ?, ?, ?);"
-            preparar = pacientes.prepare(query)
-            pacientes.execute(preparar, (nombre, apellido, rut, email, fecha_nacimiento))
 
-            query= "SELECT id FROM pacientes.paciente WHERE rut='"+rut+"' ALLOW FILTERING;"
+        query = "SELECT id FROM recetas.receta;"
+        rows = recetas.execute(query)
+        max_id_receta = 0
+        for row in rows:
+            if(max_id_receta < row.id):
+                max_id_receta = row.id
+
+        #Si el paciente no está, se ingresa tanto él/ella como la receta
+        if(rut not in lista_ruts):
+            query = "INSERT INTO pacientes.paciente(id, nombre, apellido, rut, email, fecha_nacimiento) VALUES(?, ?, ?, ?, ?, ?);"
+            preparar = pacientes.prepare(query)
+            pacientes.execute(preparar, (max_id_paciente+1, nombre, apellido, rut, email, fecha_nacimiento))
+
+            '''query= "SELECT id FROM pacientes.paciente WHERE rut='"+rut+"' ALLOW FILTERING;"
             result = pacientes.execute(query)
             for row in result:
-                id_paciente = row.id
+                id_paciente = row.id'''
 
-            query = "INSERT INTO recetas.receta(id, id_paciente, comentario, farmacos, doctor) VALUES(now(),?,?,?,?);"
+            query = "INSERT INTO recetas.receta(id, id_paciente, comentario, farmacos, doctor) VALUES(?,?,?,?,?);"
             preparar = recetas.prepare(query)
-            recetas.execute(preparar, (id_paciente, comentario, farmacos, doctor))
+            recetas.execute(preparar, (max_id_receta + 1, max_id_paciente + 1, comentario, farmacos, doctor))
+
             return render_template('create.html', valores= 'Receta y usuario ingresados exitosamente')
+
+        #Paciente ya existente, se crea solo la receta
         else:
             query= "SELECT id FROM pacientes.paciente WHERE rut='"+rut+"' ALLOW FILTERING;"
             result = pacientes.execute(query)
             for row in result:
                 id_paciente = row.id
 
-            query = "INSERT INTO recetas.receta(id, id_paciente, comentario, farmacos, doctor) VALUES(now(),?,?,?,?);"
+            query = "INSERT INTO recetas.receta(id, id_paciente, comentario, farmacos, doctor) VALUES(?,?,?,?,?);"
             preparar = recetas.prepare(query)
-            recetas.execute(preparar, (id_paciente, comentario, farmacos, doctor))
+            recetas.execute(preparar, (max_id_receta + 1, id_paciente, comentario, farmacos, doctor))
+
             return render_template('create.html', valores= 'Receta ingresada exitosamente')
 
     return render_template('create.html')
-@app.route('/edit',methods = ['POST'])
+@app.route('/update',methods = ['GET', 'POST'])
 def edit_receta():
     pacientes, recetas = cassandra()
     if request.method == 'POST':
-        return render_template()
-    return render_template()
-@app.route('/delete',methods = ['POST'])
+
+        for key, value in request.form.items():
+            if key != 'id':
+                query = "UPDATE recetas.receta SET "+key+" = '"+value+"' WHERE id = "+id_receta+";"
+                recetas.execute(query)
+            else:
+                id_receta = value
+        '''query = "UPDATE recetas.receta set WHERE ALLOW FILTERING;"
+        for key, value in request.form.items():
+
+        id_receta = request.form['id_receta']
+        comentario = request.form['comentario']
+        farmacos = request.form['farmacos']
+        doctor = request.form['doctor']'''
+        return render_template('update.html', valor = 'Receta actualizada')
+    return render_template('update.html')
+
+@app.route('/delete', methods = ['GET', 'POST'])
 def delete_receta():
     pacientes, recetas = cassandra()
     if request.method == 'POST':
-        return render_template()
-    return render_template()
+        id_receta = request.form['id_receta']
+        query = "DELETE FROM recetas.receta WHERE id ="+id_receta+" ;"
+        recetas.execute(query)
+    return render_template('delete.html', valor= 'Receta eliminada exitosamente')
 
 if __name__=='__main__':
     app.run(debug=True,host='0.0.0.0',port=5000)
